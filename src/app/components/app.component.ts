0e52205116ba66f4f85c1ecf1e2876e70baf26e0
Thanh Nguyen <Sun.SmartBiz@gmail.com>
2016-04-13 21:34:18 +0700

    #1.0.3. Icon Fonts icomoon_free font_awesome Bootstrap DropDown

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,15 +1,16 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
-import {NavbarComponent}  from './navbar.component';
-import {ToolbarComponent} from './toolbar.component';
-import {HomeComponent}    from '../../home/components/home.component';
-import {AboutComponent}   from '../../about/components/about.component';
-import {NameListService}  from '../../shared/services/name-list.service';
+import {NavbarComponent}   from './navbar.component';
+import {ToolbarComponent}  from './toolbar.component';
+import {HomeComponent}     from '../../home/components/home.component';
+import {AboutComponent}    from '../../about/components/about.component';
+import {NameListService}   from '../../shared/services/name-list.service';
 
-import {SearchComponent}  from '../../search/components/search.component';
-import {SearchService}    from '../../shared/services/search.service';
-import {EditComponent}    from '../../search/components/edit.component';
+import {SearchComponent}   from '../../search/components/search.component';
+import {SearchService}     from '../../shared/services/search.service';
+import {EditComponent}     from '../../search/components/edit.component';
 
+// import {BootstrapComponent} from '../../bootstrap/components/bootstrap.component';
 
 @Component({
   selector: 'sd-app',
@@ -18,9 +19,9 @@ import {EditComponent}    from '../../search/components/edit.component';
   directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
 })
 @RouteConfig([
-  { path: '/',           name: 'Home',   component: HomeComponent  },
-  { path: '/about',      name: 'About',  component: AboutComponent },
-  { path: '/search',     name: 'Search', component: SearchComponent },
-  { path: '/edit/:name', name: 'Edit',   component: EditComponent }
+  { path: '/',           name: 'Home',        component: HomeComponent  },
+  { path: '/about',      name: 'About',       component: AboutComponent },
+  { path: '/search',     name: 'Search',      component: SearchComponent },
+  { path: '/edit/:name', name: 'Edit',        component: EditComponent }
 ])
 export class AppComponent {}
